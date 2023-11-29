96181475e93258a8e53390efe3e2ceea657ddd5f
AmyMcCann <amy.mccann.34@gmail.com>
2023-11-29 17:05:06 +0000

    Set Project folder up ready for blog posts and tags work

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
 from lib.database_connection import DatabaseConnection
-from lib.post_repository import PostRepository
 
 class Application():
     def __init__(self):
         self._connection = DatabaseConnection()
         self._connection.connect()
-        self._connection.seed("seeds/blog.sql")
-
-    def run(self):
-        post = PostRepository(self._connection)
-        result = post.find_with_comments(4)
-        print(result)
-
-if __name__ == '__main__':
-    app = Application()
-    app.run()
+        self._connection.seed("seeds/blog_posts_tags.sql")
